2f5caf2c3348122110fe644e8af08c91da1ac4bc
Taeuk Kang <iamtaeuk@gmail.com>
2017-03-18 11:31:53 +0900

    string2int.c works!

--- string2int.c
+++ string2int.c
@@ -4,76 +4,36 @@
 int str2int();
 
 int main() {
-    
-    str2int("987654321");     // '1', '2', '3', '4'
-    
-    return 0;
-    
+
+	str2int("1235526");
+
+	return 0;
+
 }
 
-("87324a347983")
-
-"Not a number"
-
-
 int str2int(char* input) {
-    
-    int output = 0;
-    int multiplier = 1;
-    
-    for (int j = 0; j < strlen(input); j++) {
-        
-    }
-    
-    /*
-    
-    for (int j = 0; j < strlen(input); j++) {
-        if (input[j] >= 48 && input[j] <= 57) {
-            
-            if (j != 0) {
-                
-                for (int k = 0; k < j; k++) {
-                    multiplier *= ;
-                    //printf("multiplier is %d\n", multiplier);
-                }
-            
-                output += (multiplier * (input[j] - 48));
-                multiplier = 1;
-            } else {
-                output = input[j] - 48;
-            }            
-            
-        }        
-    }
-    */
-    
-    /* 
-    for (int j = 0; j < strlen(input); j++) {
-        if (input[j] >= 48 && input[j] <= 57) {
-            
-            for (int k = 0; k < j; k++) multiplier *= 10;
-            output += multiplier * (input[j] - 48);
-            
-        }        
-    }
-    
-    */
-    /*
-    for (int j = strlen(input); j >= 0; j--) {
-        if (input[j] >= 48 && input[j] <= 57) {
-            
-            for (int k = j; k >= 0; k--) multiplier *= 10;
-            printf("multiplier is %d\n", multiplier);
-            output += multiplier * (input[j] - 48);
 
-            
-        }        
-    }
-    */
-    
-    printf("output = %d\r\n", output);
-    
-    return 1;
+	int output = 0;
+	int multiplier = 1;
+
+	for (int i = 0; i < strlen(input); i++) {
+		if (input[i] >= 48 && input[i] <= 57) {
+			// DEBUG: printf(" = %d \n", strlen(input) - (i + 1));
+			for (int j = 0; j < strlen(input) - (i + 1); j++) {
+				multiplier *= 10;
+			}
+			output += (input[i] - 48) * multiplier;
+			// DEBUG: printf("multiplier = %d \n", multiplier);
+			multiplier = 1;
+		} else {
+			printf("Error! - NaN \n");
+			exit();
+		}
+	}
+
+	printf("output = %d\r\n", output);
+
+	return 1;
 }
 
 //    convert string number to integer        "5" + "1" =  "51"
