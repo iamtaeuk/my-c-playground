3dc7ef4573d5af73b61218c73b5bd0c4c0709ef4
Taeuk Kang <iamtaeuk@gmail.com>
2017-06-25 11:33:27 +0900

    Update double-link-list.c
    Incomplete!
    
    TODO: fix tail linking issue

--- double-link-list.c
+++ double-link-list.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define TRUE 1
+
 typedef struct _node{
   int data;
   struct _node *prev;
@@ -15,7 +17,7 @@ typedef struct _list{
 
 
 
-NODE allocNode(int data){
+NODE allocNode (int data){
   NODE node = (NODE)malloc(sizeof(NODE));
   node->data = data;
   node->prev = NULL;
@@ -23,27 +25,49 @@ NODE allocNode(int data){
   return node;
 }
 
-void Initialize(List *lt){
+void Initialize (List *lt){
   lt->head = NULL;
   lt->tail = NULL;
 }
 
-void Add(List *lt, int data){
+void Uninitialize (List *lt) {
+  NODE temp = lt->head;
+  free(lt->head);
+  for (int i = 0; i < 5; i++){
+  //  printf("%d\n",temp->data);
+    temp = temp->next->next;
+  //  printf("temp->prev: %d \n", temp->prev->data);
+    temp->prev->data = 0;
+    free(temp->prev);
+  }
+}
+
+void Add (List *lt, int data){
   NODE node = allocNode(data);
   if (lt->head == NULL) {
     lt->head = lt->tail = node;
-    lt->head->next = lt->tail;
     lt->tail->prev = lt->head;
+  } else if (lt->head == lt->tail) {
+    node->prev = lt->head;
+    lt->head->next = lt->tail;
+    lt->tail = node;
   } else {
-    node->next = lt->tail;
+    lt->tail->prev->next = node;
     node->prev = lt->tail->prev;
-    lt->tail->prev = node;
-    lt->tail->prev->prev->next = node;
+    lt->tail = node;
   }
   
 }
 
-int main() {
+
+void Print(List *lt){
+  NODE temp = lt->head;
+  for(; temp == lt->tail ; temp = temp->next)
+    printf("%d\n", temp->data);
+}
+
+
+int main(void) {
   
   List lt;
   Initialize(&lt);
@@ -51,7 +75,24 @@ int main() {
   Add(&lt, 10);
   Add(&lt, 20);
   Add(&lt, 30);
-  printf("%d \n", lt.head->data);
+
+  //Print(&lt);
   
+  //printf("%d \n", lt.head->next->next->next->data);
+  /*
+  NODE one = lt.head;
+  NODE two = lt.head->next;
+  NODE three = lt.head->next->next;
+  
+  printf("before: %d %d %d \n", one->data, two->data, three->data);
+  */
+  //Uninitialize(&lt);
+
+  
+ // printf("\n\nAfter: %d %d %d \n", one->data, two->data, three->data);
+  
+  printf("30: %d \n", lt.tail->data);
+  printf("20: %d \n", lt.tail->prev->data);
+  printf("10: %d \n", lt.tail->prev->prev->data);
   return 0;
 }
