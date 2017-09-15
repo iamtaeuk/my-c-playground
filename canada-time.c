c9399e588a581df66ea421dc0d117a4483891af7
Taeuk Kang <iamtaeuk@gmail.com>
2017-09-15 20:48:49 +0900

    Update canada-time.c
    line 18: stjohns algorithm does not calculate properly!

--- canada-time.c
+++ canada-time.c
@@ -3,19 +3,22 @@
 // To execute C, please define "int main()"
 
 int main() {
-  int ottawa = 145; // 00 01  24 - 
+  int ottawa = 2300; // 00 01  24 - 
   
   // 1017 hr = 10, min = 17
   
+  int hrs  = ottawa / 100;
+  int min  = ottawa % 100;
   
-  int victoria = 2400 - (300 - ottawa);
-  int edmonton = 2400 - (200 - ottawa);
-  int winnipeg = 2400 - (100 - ottawa);
-  int toronto  = ottawa + 000;
-  int halifax  = ottawa < 2300 ? ottawa + 100 : ottawa % 100;
-  int stjohns  = ottawa < 2300 ? (ottawa + 130 < 59 ? ottawa + 130 : ottawa + 130 + (60 - ottawa % 100) ): (ottawa % 100) + 30 < 59 ? (ottawa % 100) + 30 : 100 + (ottawa % 100);
+  int victoria =  (((hrs - 3) * 100) > 0 ? ((hrs - 3) * 100) : 2400 - (3 * 100))+ min; // Ottawa -3 ; 2345
+  int edmonton =  (((hrs - 2) * 100) > 0 ? ((hrs - 2) * 100) : 2400 - (2 * 100))+ min;
+  int winnipeg =  (((hrs - 1) * 100) > 0 ? ((hrs - 1) * 100) : 2400 - (1 * 100))+ min;
+  int toronto  =  (((hrs - 0) >  0 ? (hrs - 0) : ((24 - hrs) < 24 ? (24 - hrs) : 0) - 0) * 100) + min; // Toronto = Ottawa
+  int halifax  =  (((hrs + 1) < 23 ? (hrs + 1) : 0) * 100) + min;
+  int stjohns  =  (min + 30) > 60 ? 100 + (min - 30) : ((hrs + 1) < 23 ? (hrs + 1) : 1) * 100 + (min + 30);
   
-  printf("Ottawa: %d\n", ottawa);
+  printf("Ottawa: %d (%d:%d) \n", ottawa, hrs, min);
   printf("%d %d %d %d %d %d\n", victoria, edmonton, winnipeg, toronto, halifax, stjohns);
+  
   return 0;
 }
